26a44c44d324a34b92bd5ee81c30a6d6394d183d
20163693 <20163693@pclab-ubuntu12.(none)>
2018-10-22 08:47:41 +0700

    okok

--- w4/2_3_4_tree.h
+++ w4/2_3_4_tree.h
@@ -1,6 +1,22 @@
 #ifndef __2_3_4_TREE_H__
 #define __2_3_4_TREE_H__
 
+typedef BTree_node* (*New_node_func)(void *, void *, int, int);
+
+typedef struct _BTree_node {
+  void *key;
+  void *payload;
+  int min_degree;
+  int current_num_key;
+  int is_leaf;
+  _BTree_node **node_list;
+} BTree_node
+
+typedef struct {
+  BTree_node *root;
+  int min_degree;
+  New_node_func new_node;
+} BTree;
 
 
 #endif
--- w4/2_3_4_tree_test.c
+++ w4/2_3_4_tree_test.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../lib/array_functions.h"
 #include "./2_3_4_tree.h"
+
+BTree_node *new_BTree_node(void *key, void *payload, int min_degree, int is_leaf) {
+  BTree_node *ret = (BTree_node *)malloc(sizeof(BTree_node));
+  ret->min_degree = min_degree;
+  ret->key = key; ret->payload = payload;
+  ret->is_leaf = is_leaf;
+}
 
 int main() {
   
