4cdf9faeab6642c9d0a5e7a59eb36ca0c7aee0a6
Đào Ngọc Thành <daongocthanh98hy@gmail.com>
2019-01-07 11:36:45 +0700

    okok

--- wfinal/main.c
+++ wfinal/main.c
@@ -56,41 +56,219 @@ void printArray(int array[], int nmemb) {
     return;
 }
 
-int main() {
-    int op = 0;
+typedef struct {
+    int x;
+    int y;
+} State;
+
+int m, n;
+
+int isStateExist(int x, int y, State *states, int nstates) {
+    for (int i = 0; i < nstates; i++) {
+        if (x == states[i].x && y == states[i].y) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void generateStates(int x, int y, State *states, int *nstates) {
+    if (isStateExist(x, y, states, *nstates)) return;
+
+    states[*nstates].x = x;
+    states[*nstates].y = y;
+    *nstates = *nstates + 1;
+
+    if (x == 0) {
+        generateStates(m, y, states, nstates);
+    }
+
+    if (y == 0) {
+        generateStates(x, n, states, nstates);
+    }
+
+    if (x != 0) {
+        generateStates(0, y, states, nstates);
+    }
+
+    if (y != 0) {
+        generateStates(x, 0, states, nstates);
+    }
+
+    if (x < m) {
+        generateStates(m, y, states, nstates);
+    }
+
+    if (y < n) {
+        generateStates(x, n, states, nstates);
+    }
+
+    int currCapOfbot1 = m - x;
+    int currCapOfbot2 = n - y;
+    if (currCapOfbot1 >= y) {
+        generateStates(x + y, 0, states, nstates);
+    }
+    if (currCapOfbot1 < y) {
+        generateStates(m, y - currCapOfbot1, states, nstates);
+    }
+
+    if (currCapOfbot2 >= x) {
+        generateStates(0, y + x, states, nstates);
+    }
+    if (currCapOfbot2 < x) {
+        generateStates(x - currCapOfbot2, n, states, nstates);
+    }
+}
+
+void printStates(State states[], int nstates) {
+    for (int i = 0; i < nstates; i++) {
+        printf("(%d, %d) \n", states[i].x, states[i].y);
+    }
+    printf("\n");
+    return;
+}
+
+void printAllPossibleNextStates(int x, int y) {
     
+    if (x == 0) {
+        printf("(%d, %d)\n", m, y);
+    }
+
+    if (y == 0) {
+        printf("(%d, %d)\n", x, n);
+    }
+
+    if (x != 0) {
+        printf("(%d, %d)\n", 0, y);
+    }
+
+    if (y != 0) {
+        printf("(%d, %d)\n", x, 0);
+    }
+
+    if (x < m) {
+        printf("(%d, %d)\n", m, y);
+    }
+
+    if (y < n) {
+        printf("(%d, %d)\n", x, n);
+    }
+
+    int currCapOfbot1 = m - x;
+    int currCapOfbot2 = n - y;
+    if (currCapOfbot1 >= y) {
+        printf("(%d, %d)\n", x + y, 0);
+    }
+    if (currCapOfbot1 < y) {
+        printf("(%d, %d)\n", m, y - currCapOfbot1);
+    }
+
+    if (currCapOfbot2 >= x) {
+        printf("(%d, %d)\n", 0, y + x);
+    }
+    if (currCapOfbot2 < x) {
+        printf("(%d, %d)\n", x - currCapOfbot2, n);
+    }
+    return;
+}
+
+int printAllPossibleStates(Graph *g, State *states) {
+    printf("The capacitor of bottle 1: ");
+    scanf(" %d", &m);
+    printf("The capacitor of bottle 2: ");
+    scanf(" %d", &n);
+
+    int nvertices = (n + 1) * (m + 1);
+    printf("Total number of verties : %d\n", nvertices);
+
+    int nstates = 0;
+    generateStates(0, 0, states, &nstates);
+    printf("Total number of edges : %d\n", nstates);
+
+    printStates(states, nstates);
+    return nstates;
+}
+
+void printResult(State states[]) {
+    int k;
+    int nstates;
+    printf("k = ");
+    scanf("%d", &k);
+    generateStates(0, 0, states, &nstates);
+
+    int targetIndex = 0;
+    for (int i = 0; i < nstates; i++) {
+        if (states[i].x == k || states[i].y == k) {
+            targetIndex = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i <= targetIndex; i++) {
+        printf("(%d, %d)\n", states[i].x, states[i].y);
+    }
+    return;
+}
+
+int main() {
+    char op = 0;
+    State *states = (State *)malloc(1000 * sizeof(State));
+    Graph *g = (Graph *)malloc(sizeof(Graph));
+    init_graph(g, true);
+
+    int nstates;
+    for (int i = 0; i < 1000; i++) {
+        states[i].x = 0; 
+        states[i].y = 0;
+    }
+
+    int a;
+    int b;
+
     do {
         printf("\n---------------------------------------\n");
-        printf("1. \n");
-        printf("2. \n");
-        printf("3. \n");
-        printf("4. \n");
-        printf("5. \n");
+        printf("1. Print out the total of vertices and edges. \n");
+        printf("2. Print out possible next state.\n");
+        printf("3. Print out total of reachable states.\n");
+        printf("4. Print out one solution for taking out k liters of water.\n");
+        printf("5. Print out the best solution.\n");
         printf("Your option: ");
-        scanf("%d", &op);
+        scanf("%c", &op);
         clearBuffer();
         printf("-----------------------------------------\n");
         
         switch(op) {
-            case 1:
+            case '1':
+                nstates = printAllPossibleStates(g, states);
+                clearBuffer();
                 break;
-            case 2:
-              
+            case '2':
+                printf("a = ");
+                scanf("%d", &a);
+                printf("b = ");
+                scanf("%d", &b);
+                printAllPossibleNextStates(a, b);
+                clearBuffer();
                 break;
-            case 3:
+            case '3':
+
                 break;
-            case 4:
+            case '4':
+                printResult(states);
+                clearBuffer();
                 break;
-            case 5:
+            case '5':
+                printResult(states);
+                clearBuffer();
                 break;
-            case 6:
+            case '6':
                 printf("Goodbye!\n");
                 break;
             default:
                 printf("Syntax error! Please input again!\n");
                 break;
         }
-    } while (op != 6);
+    } while (op != '6');
 
     return 0;
 }
